f6756a3d0e78e607229ec94946fcc957942ac00a
Roaa <roabassem076@gmail.com>
2025-07-04 01:44:59 +0200

    added lessons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+﻿import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import AllLessons from './pages/AllLessons';
@@ -9,22 +8,24 @@ import Profile from "./pages/Profile";
 import Exams from "./pages/Exams";
 import StudentExam from "./pages/StudentExam";
 import Navbar from "./components/Navbar";
-
+import AddLessonForm from './pages/AddLessonForm';
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/lessons" element={<AllLessons />} />
-        <Route path="/lesson/:id" element={<LessonDetails />} />
-        <Route path="*" element={<NotFound />} /> 
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/exams" element={<Exams />} />
-        <Route path="/student-exam/:id" element={<StudentExam />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/lessons" element={<AllLessons />} />
+                <Route path="/lesson/:id" element={<LessonDetails />} />
+                <Route path="/add-lesson" element={<AddLessonForm />} /> 
+                <Route path="/edit-lesson/:id" element={<AddLessonForm />} /> 
+                <Route path="/exams" element={<Exams />} />
+                <Route path="/student-exam/:id" element={<StudentExam />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
