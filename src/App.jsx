107babbc233518b03d38f45d4a050117b6c88612
Mohamed-Adel97 <128996656+Mohamed-Adel97@users.noreply.github.com>
2025-07-07 18:55:46 +0300

    Designed custom dashboard UI, integrated it with the API, and implemented pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
 import Home from "./pages/Home";
 import AllLessons from './pages/AllLessons';
 import LessonDetails from './pages/LessonDetails';
 import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
+import TotalUsing from "./components/DashBord/AdminPannel/TotalUsing";
+import DashBord from "./components/DashBord/AdminPannel/DashBord";
+import Users from "./components/DashBord/AdminPannel/Users";
+import Maps from "./components/DashBord/AdminPannel/Maps";
 
 function App() {
   return (
@@ -16,6 +18,13 @@ function App() {
         <Route path="/lesson/:id" element={<LessonDetails />} />
         <Route path="*" element={<NotFound />} /> 
         <Route path="/profile" element={<Profile/>} />
+         {/* Here Related By Page Admin-DashBord */}
+          <>
+          <Route path="/admin/dashbord" element={<DashBord/>} />
+          <Route  path="/admin/dashbord"  element={<DashBord />}  children={<Route path="/admin/dashbord" element={<TotalUsing />} />}/>
+          <Route path="/admin/dashbord" element={<DashBord />} children={   <Route path="/admin/dashbord/users" element={<Users />} /> }/>
+          <Route  path="/admin/dashbord"  element={<DashBord />}  children={<Route path="/admin/dashbord/maps" element={<Maps />} />}/>
+        </>
       </Routes>
     </Router>
   );
